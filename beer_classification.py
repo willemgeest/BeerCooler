1b16cc6a3d7f635ed4e3291d5da32428388c55f1
Willem van der Geest <41240859+willemgeest@users.noreply.github.com>
2020-12-09 13:15:02 +0100

    Update beer_classification.py

--- beer_classification.py
+++ beer_classification.py
@@ -20,8 +20,7 @@ def get_class_model_Drive():
     save_dest.mkdir(exist_ok=True)
     f_checkpoint = Path("checkpoints/resnet50-19c8e357.pth")
     if not f_checkpoint.exists():
-        with st.spinner("Downloading classification model... this may take a while! \n Don't stop it!"):
-            download_file_from_google_drive('1BhJaGO6ENvk5va8zVaSJsl8XFCVckCu6', f_checkpoint)
+        download_file_from_google_drive('1BhJaGO6ENvk5va8zVaSJsl8XFCVckCu6', f_checkpoint)
 
     model = resnet50(pretrained=True)
     return model
